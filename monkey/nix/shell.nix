7511ec549e0eca3861c305e8dee0ce80981f5815
Evgeni Zhukov <zhukov@soramitsu.co.jp>
2021-02-11 14:42:05 +0700

    Add flutter to shell.nix

--- monkey/nix/shell.nix
+++ monkey/nix/shell.nix
@@ -1,21 +1,45 @@
 let
+
   mozillaOverlay =
     import (builtins.fetchGit {
       url = "https://github.com/mozilla/nixpkgs-mozilla.git";
       rev = "18cd4300e9bf61c7b8b372f07af827f6ddc835bb";
     });
+
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
+
   rust-nightly = with nixpkgs; ((rustChannelOf { date = "2020-10-23"; channel = "nightly"; }).rust.override {
     targets = [ "wasm32-unknown-unknown" ];
     extensions = ["rust-src"];
   });
+
+  flutterPkgs =
+    import (builtins.fetchGit {
+      url = "https://github.com/babariviere/nixpkgs.git";
+      rev = "fc592a52cacfbf5f22e6479a22263983f5346ea6";
+  });
+
+  dartPkgs =
+    import (builtins.fetchGit {
+      url = "https://github.com/GRBurst/nixpkgs.git";
+      rev = "47639c945ea305be65abce5649f61529e5b6f011";
+  });
+
 in
 with nixpkgs; pkgs.mkShell {
   buildInputs = [
+
+    git
+
     clang
     cmake
     pkg-config
     rust-nightly
+
+    flutterPkgs.flutter
+    dartPkgs.dart
+    android-studio
+
   ] ++ stdenv.lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
   ];
